75ded047f4fe8c585eec0004581fafbecf8bf6a3
17kbeans <17beans@naver.com>
2023-08-23 01:17:33 +0900

    Fix type error res.params.gameID string to number

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const port = 3000;
 import { gameIDs } from './PeriodicFunctionSystem/AuthenticatedGameIDs.js';
 import { LuaCodePeriodicFunctinSystem } from './PeriodicFunctionSystem/PeriodicFunctionSystem.js';
 const allGameIDs = JSON.parse(gameIDs);
-console.log(`allGameIDs: ${allGameIDs}`);
 
 
 
@@ -18,17 +17,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/PeriodicFunctionSystem/:gameID', (req, res) => {
-    const gameID = req.params.gameID
+    const gameID = Number(req.params.gameID)
     if (!gameID) {
         return res.json({
             success: false,
             status: 'Invalid request',
         });
     }
-    console.log(`gameIDs: ${gameIDs}`);
+    console.log(`gameIDs: ${allGameIDs}`);
     console.log(`gameID: ${gameID}`);
-    console.log(`gameIDs.includes(gameID): ${gameIDs.includes(gameID)}`);
-    if (!gameIDs.includes(gameID)) {
+    console.log(`allGameIDs.includes(gameID): ${allGameIDs.includes(gameID)}`);
+    if (!allGameIDs.includes(gameID)) {
         return res.json({
             success: false,
             status: 'Authentication failed',
