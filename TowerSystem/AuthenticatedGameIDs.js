645164d88f4d31aa268ead75883d9ac2aaf30bf4
17kbeans <17beans@naver.com>
2023-08-28 21:16:38 +0900

    Update AuthenticatedGameIDs.js

--- TowerSystem/AuthenticatedGameIDs.js
+++ TowerSystem/AuthenticatedGameIDs.js
@@ -1,5 +1,7 @@
 export const gameIDs = JSON.stringify(
     [
+        // TowerSystem Experience
+        4680268414,
         // Crong Studio Natural Disaster Tower Experience
         4992847527,
     ]
