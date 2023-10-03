d1afba12b5a0f0f296c485e65d3e0e0174f04632
17kbeans <17beans@naver.com>
2023-10-03 15:23:24 +0900

    Update AuthenticatedGameIDs.js

--- snszmfl-SnowballFightTower/AuthenticatedGameIDs.js
+++ snszmfl-SnowballFightTower/AuthenticatedGameIDs.js
@@ -3,6 +3,6 @@ export const gameIDs = JSON.stringify(
         // bean7189 Test Experience
         4871036727,
         // snszmfl User SnowballFightTower Experience
-        5150020826,
+        5151467428,
     ]
 );
