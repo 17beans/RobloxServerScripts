9762711f5a2f0e5ae486016a692759c8759ed1ea
17kbeans <17beans@naver.com>
2023-10-07 22:25:32 +0900

    Update LocalizationSystem.js

--- snszmfl-SnowballFightTower/LocalizationSystem.js
+++ snszmfl-SnowballFightTower/LocalizationSystem.js
@@ -10,74 +10,30 @@ export const LuaCodeLocalizationSystem = `
 
 
 --// Service //--
-local ReplicatedStorage = game:GetService('ReplicatedStorage')
-local RunService = game:GetService('RunService')
-local Players = game:GetService('Players')
+
 ----
 
 
 
 --// Presetup //--
-local FolderAutoSetup = script:FindFirstChild('AutoSetup')
-if FolderAutoSetup then
-	for _, Service in pairs(FolderAutoSetup:GetChildren()) do
 
-		if Service.Name == 'StarterPlayer' then
-			local targetService = game[Service.Name]
-
-			for _, StarterScripts: Folder in pairs(Service:GetChildren()) do
-				--warn('[Service] Folder: ' .. tostring(Folder))
-				for _, target in pairs(StarterScripts:GetChildren()) do
-					local Location = targetService[StarterScripts.Name]
-					target.Parent = Location
-				end
-			end
-
-		else
-
-			local Location = game[Service.Name]
-			if not Location then continue end
-
-			for _, target in pairs(Service:GetChildren()) do
-				target.Parent = Location
-			end
-
-		end
-	end
-end
 ----
 
 
 
 --// Types //--
-type TypeLocaleID = {
-	['ko-kr']: string,
-	['en-us']: string,
-}
 
-local LocaleIDs = {
-	['ko-kr'] = 'ko-kr',
-	['en-us'] = 'en-us',
-}
 ----
 
 
 
 --// Constants //--
-local fLocalizationSystemReplicated = ReplicatedStorage.LocalizationSystem
-local LocaleIDHandler = script.LocaleIDHandler
 
-local config = script.Config
-
-local DefaultLocaleID = LocaleIDs["ko-kr"]
-
-local IsStudio = RunService:IsStudio()
 ----
 
 
 
---// Remotes //--
-local REvtSendLocaleID = fLocalizationSystemReplicated.SendLocaleID
+--// Remotes / Bindables //--
 
 ----
 
@@ -90,82 +46,48 @@ local REvtSendLocaleID = fLocalizationSystemReplicated.SendLocaleID
 
 
 --// Variables //--
-local playerAddedPassed = {}
-local SupportLocaleIDs = {}
+local playerAdded = {}
 ----
 
 
 
 --// Functions //--
 local function OnPlayerAdded(player: Player)
-	table.insert(playerAddedPassed, player)
+	playerAdded[player] = true
 
-	local fLocalizationSystem = Instance.new('Folder')
-	fLocalizationSystem.Parent = player
-	fLocalizationSystem.Name = 'LocalizationSystem'
-
-
-	local PlayerGui = player:WaitForChild('PlayerGui', 5)
-	local connection: RBXScriptConnection
-	connection = fLocalizationSystem.ChildAdded:Connect(function(child)
-		if child.Name == 'LocaleID' then
-			connection:Disconnect()
-			connection = nil
-			local Found_LocaleIDHandler = PlayerGui:FindFirstChild('LocaleIDHandler')
-			if Found_LocaleIDHandler then Found_LocaleIDHandler:Destroy() end
-		end
+	local vLocaleID = Instance.new('StringValue')
+	vLocaleID.Parent = player
+	vLocaleID.Name = 'LocaleID'
+	local s, e = pcall(function()
+		vLocaleID.Value = player.LocaleId
 	end)
-
-	local Cloned_LocaleIDHandler = LocaleIDHandler:Clone()
-	Cloned_LocaleIDHandler.Parent = PlayerGui
+	if not s then
+		warn('Localization error: Failed to create LocaleID')
+	end
 end
 
--- PlayerAdded 이벤트가 발생하기 전 이미 접속 완료된 플레이어가 존재할 경우에 대한 예외 처리 함수
-local function CheckPlayerAddedPassedPlayer()
-	for _, player in Players:GetPlayers() do
-		local passed = table.find(playerAddedPassed, player)
-		if passed then continue end
-
-		table.insert(playerAddedPassed, player)
+local function CheckPlayerAdded()
+	for _, player in pairs(game.Players:GetPlayers()) do
+		if playerAdded[player] then continue end
 		OnPlayerAdded(player)
 	end
 end
 
-
-local function OnReceiveLocaleIDFromClient(player: Player, LocaleID: TypeLocaleID)
-	local fLocalizationSystem: Folder = player.LocalizationSystem
-
-	local vLocaleID = Instance.new('StringValue')
-	vLocaleID.Parent = fLocalizationSystem
-	vLocaleID.Name = 'LocaleID'
-
-	if not table.find(SupportLocaleIDs, LocaleID) then
-		print('[Server] Player LocaleID is not support: ' .. tostring(LocaleID))
-	end
-
-
-	if IsStudio then
-		--print('[Server] 기본 LocaleID로 설정합니다: ', tostring(DefaultLocaleID))
-		vLocaleID.Value = DefaultLocaleID
-	else
-		vLocaleID.Value = 
-			table.find(SupportLocaleIDs, LocaleID) and LocaleID or DefaultLocaleID
-	end
+local function OnPlayerRemoving(player: Player)
+	playerAdded[player] = nil
 end
 ----
 
 
 
 --// Setup //--
-for _, LocaleID: BoolValue in pairs(config.SupportLocaleIDs:GetChildren()) do
-	table.insert(SupportLocaleIDs, LocaleID.Name)
-end
+
 ----
 
 
 
 --// Main //
-REvtSendLocaleID.OnServerEvent:Connect(OnReceiveLocaleIDFromClient)
-Players.PlayerAdded:Connect(OnPlayerAdded)
-CheckPlayerAddedPassedPlayer()
+game.Players.PlayerAdded:Connect(OnPlayerAdded)
+game.Players.PlayerRemoving:Connect(OnPlayerRemoving)
+CheckPlayerAdded()
 ----`
