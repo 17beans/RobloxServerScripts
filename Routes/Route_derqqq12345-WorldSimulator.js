8681e18ad412e1d76a9d459665f298303b576d59
17kbeans <17beans@naver.com>
2023-11-15 20:29:32 +0900

    Create Route_derqqq12345-WorldSimulator.js

---
+++ Routes/Route_derqqq12345-WorldSimulator.js
@@ -0,0 +1,54 @@
+import { gameIDs } from "../derqqq12345-WorldSimulator/AuthenticatedGameIDs.js";
+import { LuaCodeGlobalValueSystem } from "../derqqq12345-WorldSimulator/GlobalValueSystem.js";
+
+
+
+const scripts = {
+    GlobalValueSystem: LuaCodeGlobalValueSystem,
+}
+const allGameIDs = JSON.parse(gameIDs);
+
+// console.log(`test code: `);
+// console.log(require('./Crong-PeriodicFunctionSystem/PeriodicFunctionSystem.lua'));
+
+
+
+export const Route_DaeSoon_ThrowCharacterSystem = (req, res) => {
+    const scriptName = req.params.ScriptName
+    if (!scriptName) {
+        return res.json({
+            success: false,
+            status: 'Invalid request',
+        });
+    }
+    const script = scripts[scriptName]
+    if (!script) {
+        return res.json({
+            success: false,
+            status: 'Invalid request',
+        });
+    }
+    const gameID = Number(req.params.gameID)
+    if (!gameID) {
+        return res.json({
+            success: false,
+            status: 'Invalid request',
+        });
+    }
+
+    console.log(`script: ${script}`);
+    console.log(`gameIDs: ${allGameIDs}`);
+    console.log(`gameID: ${gameID}`);
+    console.log(`allGameIDs.includes(gameID): ${allGameIDs.includes(gameID)}`);
+    if (!allGameIDs.includes(gameID)) {
+        return res.json({
+            success: false,
+            status: 'Authentication failed',
+        });
+    }
+
+    return res.json({
+        success: true,
+        code: script,
+    });
+}
