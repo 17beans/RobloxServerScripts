7b632e8c34542ce72940f08f8686f8a43d070cf4
17kbeans <17beans@naver.com>
2023-12-17 01:55:14 +0900

    Delete daesoon-TaggerTower.js

--- api/daesoon-TaggerTower.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-
-
-
-app.get('/:gameIDplaceIDcreatorID/:ScriptName', Route_DaeSoon_TaggerTower);
-app.get('/', (req, res) => {
-    return res.json("daesoon-TaggerTower")
-});
